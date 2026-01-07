b43477b8a156d0be496a528a700910aad9922473
aleksey.m <aleksey.mogilin@gridinsoft.com>
2026-01-07 04:14:18 +0200

    * magnify 1->0, 99->100

--- esphome/mix_tools.h
+++ esphome/mix_tools.h
@@ -6,8 +6,8 @@ static inline int mixer_math(uint16_t pot_raw, uint16_t vref_raw, int invert) {
         return 0;
 
     int cand = (int)((uint32_t)pot_raw * 100u / vref_raw);
-    if (cand < 0) cand = 0;
-    if (cand > 100) cand = 100;
+    if (cand < 1) cand = 0;
+    if (cand > 99) cand = 100;
 
     if (invert)
         cand = 100 - cand;
